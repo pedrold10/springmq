6c962ce639fedf9c1029fd98bff3539b49256da9
Pedro Gabriel <58262161+pedrold10@users.noreply.github.com>
2025-10-25 15:15:06 -0300

    Inicializando projeto

---
+++ messaging-spring-boot/src/main/java/com/pedro/messaging_spring_boot/MessagingSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.pedro.messaging_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagingSpringBootApplication.class, args);
+	}
+
+}
